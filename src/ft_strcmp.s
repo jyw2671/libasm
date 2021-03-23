08a92aabc76e271beaf3577afab540f61cceb424
정영웅 <yjung@jeong-yeong-ung-ui-MacBookPro.local>
2021-03-23 22:26:25 +0900

    add ft_strcpy

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -5,20 +5,20 @@ section .text
 ; ret == rax -> al
 
 _ft_strcmp:
-    push    rcx
+    push    rcx         ; 반복문에서 카운터로 사용되는 레지스터
     xor     rcx, rcx
     jmp     loop
 
 loop_inc:
-    inc     rcx
+    inc     rcx         ; jmp가 없으면 아랫줄로
 
 loop:
     cmp     BYTE[rdi+rcx], 0
     je      end
     cmp     BYTE[rsi+rcx], 0
     je      end
-    mov     al, BYTE[rdi+rcx]
-    cmp     al, BYTE[rsi+rcx]
+    mov     al, BYTE[rdi+rcx]   ; byte가 8bit 이므로
+    cmp     al, BYTE[rsi+rcx]   ; rax가 아닌 al 사용
     je      loop_inc
 
 end:
--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -1,3 +1,10 @@
 section .text
     global _ft_strcpy
+    extern _ft_strlen
 ; char    *strcpy(char * dst, const char * src);
+
+_ft_strcpy:
+    push    rbx     ; 메모리 주소를 저장하기 위한 용도로 사용
+    push    rcx     ; 반복문에서 카운터로 사용되는 레지스터
+
+loop:

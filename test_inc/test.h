642722dd4ea8ef708b26c9aacf61c2df2c4166d4
정영웅 <yjung@jeong-yeong-ung-ui-MacBookPro.local>
2021-03-24 22:38:10 +0900

    add ft_strcmp_test func

--- test_inc/test.h
+++ test_inc/test.h
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 15:31:24 by yjung             #+#    #+#             */
-/*   Updated: 2021/03/23 15:35:59 by yjung            ###   ########.fr       */
+/*   Updated: 2021/03/24 19:19:49 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,22 @@
 
 extern char	*g_string[10];
 
+char	*ft_strcpy(char *dst, const char *src);
+void	ft_strcpy_test(void);
+
+size_t	ft_strlen(const char *s);
 void	ft_strlen_test(void);
+
+int		ft_strcmp(const char *s1, const char *s2);
+void	ft_strcmp_test(void);
+
+// ssize_t	ft_write(int fd, const char *buf, size_t count);
+// void	ft_write_test(void);
+
+// ssize_t	ft_read(int fd, const char *buf, size_t count);
+// void	ft_read_test(void);
+
+// char	*ft_strdup(const char *s1);
+// void	ft_strdup_test(void);
 
 #endif

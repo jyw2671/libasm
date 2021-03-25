24ed5397104c934b1f73b6f3e1a99f72261944b9
정영웅 <yjung@jeong-yeong-ung-ui-MacBookPro.local>
2021-03-25 15:34:14 +0900

    add read, write func

--- test_inc/test.h
+++ test_inc/test.h
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 15:31:24 by yjung             #+#    #+#             */
-/*   Updated: 2021/03/24 19:19:49 by yjung            ###   ########.fr       */
+/*   Updated: 2021/03/25 15:32:20 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ void	ft_strlen_test(void);
 int		ft_strcmp(const char *s1, const char *s2);
 void	ft_strcmp_test(void);
 
-// ssize_t	ft_write(int fd, const char *buf, size_t count);
-// void	ft_write_test(void);
+ssize_t	ft_write(int fd, const char *buf, size_t count);
+void	ft_write_test(void);
 
-// ssize_t	ft_read(int fd, const char *buf, size_t count);
-// void	ft_read_test(void);
+ssize_t	ft_read(int fd, const char *buf, size_t count);
+void	ft_read_test(void);
 
 // char	*ft_strdup(const char *s1);
 // void	ft_strdup_test(void);
